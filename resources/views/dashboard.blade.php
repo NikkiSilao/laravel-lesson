10a9123a7dbb24b41d2830060bbe1267cda62918
Nikki Lei A. Silao <nikkileiasilao123@gmail.com>
2024-05-11 16:55:51 +0800

    Activity Done Change Profile Pic! march 11

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,7 +24,7 @@
       <div class="card">
         <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
 
-          <img src="malupiton.jpg" alt="Profile" class="rounded-circle">
+          <img src="alden.jpg" alt="Profile" class="rounded-circle">
           <h2>Nikki Lei Silao</h2>
           <h3>Programmer</h3>
           <div class="social-links mt-2">
